f2ad2270715ab1ec6df1dc0eff8eee084abdde06
Matt Vollrath <matt@endpoint.com>
2015-10-15 12:44:54 -0400

    License for spacenav-emitter.c

--- src/spacenav-emitter.c
+++ src/spacenav-emitter.c
@@ -1,3 +1,25 @@
+/*
+* Copyright 2012 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/*
+** This turns on the LED of a 3DConnection Space Navigator device:
+** $ led-enable /dev/input/spacenavigator [1|0]
+** where 1 is "on" and 0 is "off"
+*/
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
